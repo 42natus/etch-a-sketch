a79fd13f417246e6527e93afa49e2214167e86b7
Fortunatus Olorunsola <fortunatus.olorunsola@gmail.com>
2026-08-31 15:48:36 +0100

    feat: generate grid based on user input

--- js/script.js
+++ js/script.js
@@ -1,13 +1,40 @@
+const button = document.querySelector("button");
 const container = document.querySelector(".container");
 
-let cellWidth = parseInt(container.offsetWidth) / 16;
-let cellHeight = parseInt(container.offsetHeight) / 16;
+button.addEventListener("click", getDimension)
+button.addEventListener("changedimension", drawGrid);
 
-for (let i = 0; i < 256; i++) {
-    const cell = document.createElement("div");
-    cell.setAttribute("class", "cell");
-    cell.setAttribute("style", `width: ${cellWidth}px; height: ${cellHeight}px;`);
-    container.appendChild(cell);
-    // change color on hover
-    cell.addEventListener("mouseenter", (event) => event.target.classList.add("change-color"));
+function drawGrid(event) {
+    clearGrid();
+    const dimension = event.detail.dimension || 16;
+    const gridSize = dimension * dimension;
+    const cellWidth = parseInt(container.offsetWidth) / dimension;
+    const cellHeight = parseInt(container.offsetHeight) / dimension;
+
+    for (let i = 0; i < gridSize; i++) {
+        const cell = document.createElement("div");
+        cell.setAttribute("class", "cell");
+        cell.setAttribute("style", `width: ${cellWidth}px; height: ${cellHeight}px;`);
+        container.appendChild(cell);
+        // change color on hover
+        cell.addEventListener("mouseenter", (event) => event.target.classList.add("change-color"));
+    }
+}
+
+function getDimension() {
+    dimension = parseInt(prompt("How many squares per side would you like?", 16));
+    // console.log(dimension);
+
+    let changeDimension = new CustomEvent("changedimension", {
+        detail: {
+            dimension: dimension,
+        },
+    });
+
+    button.dispatchEvent(changeDimension);
+}
+
+function clearGrid() {
+    const cells = container.querySelectorAll(".cell");
+    cells.forEach((cell) => cell.remove());
 }
