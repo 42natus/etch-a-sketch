0b3f5ee9c6fb6efbda0824c6899ef63a5b07e6c0
Fortunatus Olorunsola <fortunatus.olorunsola@gmail.com>
2026-08-31 11:22:02 +0100

    Create 16x16 grid on page

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector(".container");
+
+let cellWidth = parseInt(container.offsetWidth) / 16;
+let cellHeight = parseInt(container.offsetHeight) / 16;
+
+for (let i = 0; i < 256; i++) {
+    const cell = document.createElement("div");
+    cell.setAttribute("class", "cell");
+    cell.setAttribute("style", `width: ${cellWidth}px; height: ${cellHeight}px;`);
+    container.appendChild(cell);
+}
+
